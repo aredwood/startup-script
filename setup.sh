25813bda5d0534771e249dbef9cc666453d49891
Alex <aredwood@users.noreply.github.com>
2019-10-09 23:44:41 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 # install utilities
 sudo apt-get install htop -y
 sudo apt-get install nload -y
+sudo apt-get install p7zip-full -y
+sudo apt-get install zip -y
 
 # install node
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
